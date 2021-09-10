ae49a280d29290d4a644717b3b50600ff0507af0
Nick Stockton <phoenix76728@gmail.com>
2021-09-10 13:00:27 -0400

    initial commit

--- UnitTestingExercise.Tests/UnitTests.cs
+++ UnitTestingExercise.Tests/UnitTests.cs
@@ -6,50 +6,58 @@ namespace UnitTestingExercise.Tests
     public class UnitTests
     {
         [Theory]
-        [InlineData()]
+        [InlineData(1, 2, 3, 6)]
         public void Add(int num1, int num2, int num3, int expected)
         {
             //Arrange
-
+            UnitTestMethods UT = new UnitTestMethods();
             //Act
-
+            int actual= UT.Add(num1, num2, num3);
             //Assert
-
+            Assert.Equal(expected, actual);
         }
 
         [Theory]
-        [InlineData()]
+        [InlineData(10, 5, 5)]
         public void Subtract(int minuend, int subtrhend, int expected)
         {
             //Arrange
-
+            UnitTestMethods UT = new UnitTestMethods();
             //Act
+            int actual = UT.Subtract(minuend,subtrhend);
 
             //Assert
+            Assert.Equal(expected, actual);
 
         }
 
         [Theory]
-        [InlineData()]
+        [InlineData(12,12,144)]
         public void Multiply(int num1, int num2, int expected)
         {
             //Arrange
+            UnitTestMethods UT = new UnitTestMethods();
 
             //Act
+            int actual = UT.Multiply(num1, num2);
 
             //Assert
+            Assert.Equal(expected, actual);
 
         }
 
         [Theory]
-        [InlineData()]
+        [InlineData(144,12,12)]
         public void Divide(int num1, int num2, int expected)
         {
             //Arrange
+            UnitTestMethods UT = new UnitTestMethods();
 
             //Act
+            int actual = UT.Divide(num1, num2);
 
             //Assert
+            Assert.Equal(expected, actual);
 
         }
 
@@ -57,10 +65,13 @@ namespace UnitTestingExercise.Tests
         public void YourMethodName()
         {
             //Arrange
-
+            UnitTestMethods UT = new UnitTestMethods();
+            string lyric = "sorrows of the earth";
             //Act
-
+            string lyricActual = UT.YourMethodName();
+            bool equal = lyric == lyricActual;
             //Assert
+            Assert.False(equal);
 
         }
 
@@ -68,10 +79,13 @@ namespace UnitTestingExercise.Tests
         public void YourMethodName2()
         {
             //Arrange
-
+            UnitTestMethods UT = new UnitTestMethods();
+            string lyric = "your mind is a stream of colors";
             //Act
-
+            string lyricActual = UT.YourMethodName2().ToLower();
+            bool equal = lyric == lyricActual;
             //Assert
+            Assert.True(equal);
         }
     }
 }
--- UnitTestingExercise/UnitTestMethods.cs
+++ UnitTestingExercise/UnitTestMethods.cs
@@ -6,21 +6,51 @@ namespace UnitTestingExercise
         // Make sure your references are set properly
             // Righ-click on Dependencies in your .Tests project and set your reference
 
-
-
         // Create an Add method that passes 3 integers
 
+        public int Add(int x,int y,int z)
+        {
+            int sum = x + y + z;
+            return sum;
+        }
+
+
+
         // Create a Subtract method that passes 2 integers
-            // Keep track of which number is getting passed as minuend and subtrahend
+        // Keep track of which number is getting passed as minuend and subtrahend
+        public int Subtract(int minuend, int subtrahend)
+        {
+            int diff = minuend - subtrahend;
+            return diff;
+        }
+
 
         // Create a Multiply method that passes 2 integers
-
+        public int Multiply(int x, int y)
+        {
+            int prod = x * y;
+            return prod;
+        }
         // Create a Divide method that passes 2 integers
-
-
+        public int Divide(int x, int y)
+        {
+            int prod = x / y;
+            return prod;
+        }
 
         // Create 2 methods that will utilize the [Fact] tests you wrote
 
-        
+        public string YourMethodName()
+        {
+             string lyric= "Sorrows of the Earth";
+            return lyric;
+        }
+        public string YourMethodName2()
+        {
+            string lyric = "Your Mind is a Stream of Colors";
+            return lyric;
+        }
+
+
     }
 }
